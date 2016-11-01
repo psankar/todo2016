e13669160cc27ea9c40fa843cb1c2f1d8a92fca9
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-01 23:12:41 +0530

    Middleware for JWT auth check

--- server/server.go
+++ server/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 // Todo is a representation of each item that
@@ -17,12 +21,47 @@ type Todo struct {
 var todos []Todo
 var lock sync.RWMutex
 
+var hmacSampleSecret = []byte("Top secret signing key")
+
+// A middleware for validating JWT tokens
+func authCheck(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return hmacSampleSecret, nil
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.StandardClaims); ok && token.Valid {
+			log.Println(claims)
+		} else {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		// Authenticate and return JWT
 	})
 
-	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/todos", authCheck(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			// Return all the Todo items
 		} else if r.Method == http.MethodPost {
@@ -30,9 +69,9 @@ func main() {
 		} else {
 			http.NotFound(w, r)
 		}
-	})
+	})))
 
-	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/todo", authCheck(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPut {
 			// Update the particular Todo item
 		} else if r.Method == http.MethodDelete {
@@ -40,7 +79,7 @@ func main() {
 		} else {
 			http.NotFound(w, r)
 		}
-	})
+	})))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
