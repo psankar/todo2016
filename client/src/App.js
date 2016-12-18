92228f12bfeeccd67698df8592de427fdf111075
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-12-18 14:44:28 +0530

    Scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import Login from './components/login';
+import NewTask from './components/new_task';
+import TaskList from './components/task_list';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,11 +12,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <Login/>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <TaskList/>
+          <NewTask/>
         </p>
       </div>
     );
---
+++ client/src/components/task_list.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class TaskList extends Component {
+    render() {
+        return <div>TaskList</div>;
+    }
+}
+
+export default TaskList;
