7f377f2900e252e1fdde93ef5aeb6e602fc1d652
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2016-12-30 13:31:07 +0530

    Fix the validation in Signin form

--- client/src/components/signin-non-redux-form.js
+++ client/src/components/signin-non-redux-form.js
@@ -31,7 +31,7 @@ class SignIn extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log('handleSubmittttttt', this.state.username, this.state.password.value);
+        console.log('handleSubmittttttt', this.state.username, this.state.password);
         if (this.state.username.length < 1) {
             this.setState({error: "Invalid username"});
             console.log(this.state.error);
@@ -43,23 +43,26 @@ class SignIn extends Component {
             return
         }
 
+        // Reseting error
+        this.setState({error: null});
         console.log("Emit success event");
     }
 
     render() {
+        // onChange callback gets an event as arg. Need to use event.target.value
         return (
             <form onSubmit={this.handleSubmit}>
                 <input
                     placeholder="Username"
                     required
                     value={this.state.username}
-                    onChange={(username) => this.setState({username: username.value})}/>
+                    onChange={(username) => this.setState({username: username.target.value})}/>
                 <input
                     placeholder="Password"
                     type="password"
                     required
                     value={this.state.password}
-                    onChange={(password) => this.setState({password: password.value})}/>
+                    onChange={(password) => this.setState({password: password.target.value})}/>
                 <button type="submit">Sign In</button>
                 {this.renderError()}
             </form>
